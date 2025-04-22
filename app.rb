3051b17c9776f40771b8c42f9cc8ecb324c68d32
denis-gandel <gandarillas.delgado.denis@gmail.com>
2025-04-22 11:21:19 -0400

    chore: change technology

--- app.rb
+++
@@ -1,7 +0,0 @@
-require_relative 'config/environment'
-
-class App < Sinatra::Base
-  register Sinatra::Namespace
-
-  run! if app_file == $0
-end
--- config/environment.rb
+++
@@ -1,6 +0,0 @@
-require 'sinatra'
-require 'sinatra/json'
-require 'sinatra/base'
-require 'sinatra/namespace'
-
-Dir[File.join(__dir__, '*.rb')].each { |file| require file }
